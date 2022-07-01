9450a6f9145bbc5fd7617cfd56d07af29bd98bcf
tonovarela <tonovarela@live.com>
2022-07-01 18:30:10 -0500

    Se usa la libreria pdfkit para construir el pdf final"

---
+++ DAO/Material.js
@@ -0,0 +1,62 @@
+
+const { QueryTypes } = require("sequelize");
+const DAO = require("./DAO");
+class Material extends DAO {
+
+    async porID(folio) {
+        return await this.sequelize.query(`
+        SELECT a.ID_MATERIAL,
+		ANCHO,
+		ALTO,
+		MEDIANIL_ANCHO,
+		MEDIANIL_ALTO,
+    (select nombre_material from materiales_cotizador where ID_MATERIAL=a.ID_MATERIAL) as descripcionMaterial,
+	(select tipo from materiales_cotizador where id_material=a.id_material) as tipoMaterial,
+        a.MEDIDA,
+		a.MATANCHO,
+		a.MATALTO,
+		a.MATENTRAN,
+		a.ORIENTA,
+		a.ID_MATERIAL2,
+		(select nombre_material from materiales_cotizador where ID_MATERIAL=a.ID_MATERIAL2) as descripcionMaterial2,
+		(select tipo from materiales_cotizador where id_material=a.id_material2) as tipoMaterial2,
+        a.MEDIDA2,
+		a.MATANCHO2,
+		a.MATALTO2,
+		a.MATENTRAN2,
+		a.ORIENTA2,
+		a.ID_MATERIAL3,
+		(select nombre_material from materiales_cotizador where ID_MATERIAL=a.ID_MATERIAL3) as descripcionMaterial3,
+		(select tipo from materiales_cotizador where id_material=a.id_material3) as tipoMaterial3,
+        a.MEDIDA3,
+		a.MATANCHO3,
+		a.MATALTO3,
+		a.MATENTRAN3,
+		a.ORIENTA3,
+		a.ID_MATERIAL3,
+        a.MEDIDA3,
+		a.MATANCHO3,
+		a.MATALTO3,
+		a.MATENTRAN3,
+		a.ORIENTA3,
+		a.ID_MATERIAL4,
+		(select nombre_material from materiales_cotizador where ID_MATERIAL=a.ID_MATERIAL4) as descripcionMaterial4,
+		(select tipo from materiales_cotizador where id_material=a.id_material4) as tipoMaterial4,
+        a.MEDIDA4,
+		a.MATANCHO4,
+		a.MATALTO4,
+		a.MATENTRAN4,
+		a.ORIENTA4
+  FROM cotizaciones a
+  where FOLIO=:folio`,
+  //{logging:console.log,plain:false }
+            {
+				plain:false,
+                replacements: { folio },
+                type: QueryTypes.SELECT
+            }
+        );
+    }
+}
+
+module.exports = Material;
--- controllers/impresion.js
+++ controllers/impresion.js
@@ -1,56 +1,48 @@
 const { response } = require('express');
 const { jsPDF } = require('jspdf')
-
-
-const { merge } = require('merge-pdf-buffers');
-
+const { uid } = require('uid');
+//const { merge } = require('merge-pdf-buffers');
 var http = require('http');
-const doc = require('pdfkit');
-const { fdatasyncSync } = require('fs');
+const ImpresionRigido = ({ enAncho, ancho, alto }, { medianilAncho, medianilAlto, anchoPieza, altoPieza }) => {
+  const _enAncho = enAncho;
+  const _ancho = ancho;  //122;
+  const _alto = alto//244;
 
+  const _medianilAncho = medianilAncho;//1;
+  const _medianilAlto = medianilAlto;//1;
 
+  let _anchoPieza = anchoPieza// 21.5;
+  let _altoPieza = altoPieza//28;
 
-const ImpresionRigido = () => {
-  const enAncho = false;
-  const ancho = 122;
-  const alto = 244;
-
-  const medianilAncho = 1;
-  const medianilAlto = 1;
-
-  let anchoPieza = 21.5;
-  let altoPieza = 28;
-
-  const nMediaNilAncho = (medianilAncho / 2);
-  const nMediaNilAlto = (medianilAlto / 2);
-
-  anchoPieza = anchoPieza + medianilAncho;
-  altoPieza = altoPieza + medianilAlto;
-  if (!enAncho) {
-    const t = anchoPieza;
-    anchoPieza = altoPieza;
-    altoPieza = t;
+  const nMediaNilAncho = (_medianilAncho / 2);
+  const nMediaNilAlto = (_medianilAlto / 2);
+  _anchoPieza = _anchoPieza + _medianilAncho;
+  _altoPieza = _altoPieza + _medianilAlto;
+  if (!_enAncho) {
+    const t = _anchoPieza;
+    _anchoPieza = _altoPieza;
+    _altoPieza = t;
   }
   let orientation = "p";
-  if (alto < ancho) {
+  if (_alto < _ancho) {
     orientation = "l";
   }
 
   const scale = 1;
-  const doc = new jsPDF(orientation, 'cm', [alto * scale, ancho * scale]);
-  const itemsAncho = Math.trunc(ancho / anchoPieza);
-  const itemsAlto = Math.trunc(alto / altoPieza);
+  const doc = new jsPDF(orientation, 'cm', [_alto * scale, _ancho * scale]);
+  const itemsAncho = Math.trunc(_ancho / _anchoPieza);
+  const itemsAlto = Math.trunc(_alto / _altoPieza);
 
   doc.setFont('times', 'bold');
-  doc.setFontSize(10);
-  doc.text(5 * scale, (alto * scale) / 2, `${alto} cm  -( ${itemsAlto} renglones) `, { angle: 90, align: "left" });
-  doc.text((ancho * scale) / 2, 5 * scale, `${ancho} cm  - (${itemsAncho} columnas) `, { align: "center" });
+  doc.setFontSize(100);
+  doc.text(5 * scale, (_alto * scale * 1.5) / 2, `${_alto} cm  -( ${itemsAlto} renglones) `, { angle: 90 });
+  doc.text((_ancho * scale) / 2, 5 * scale, `${_ancho} cm  - (${itemsAncho} columnas) `, { align: "center" });
 
   var piezas = itemsAncho * itemsAlto;
   var pageHeight = doc.internal.pageSize.height || doc.internal.pageSize.getHeight();
   var pageWidth = doc.internal.pageSize.width || doc.internal.pageSize.getWidth();
 
-  doc.text(`${piezas} items (${anchoPieza}cm x ${altoPieza} cm) `, pageWidth / 2, pageHeight / 2, { align: 'center' });
+  doc.text(`${piezas} items (${_anchoPieza}cm x ${_altoPieza} cm) `, pageWidth / 2, pageHeight / 2, { align: 'center' });
 
   doc.setDrawColor(98, 5, 100,);
 
@@ -60,109 +52,166 @@ const ImpresionRigido = () => {
     incrementadory = 0;
     incrementadorx += nMediaNilAncho * 1;
     for (let j = 0; j <= (itemsAlto - 1); j++) {
-      incrementadory += (medianilAlto / 2) * 1;
-      doc.setLineWidth(0.5);
-      doc.rect((i * ((anchoPieza * scale) - (nMediaNilAncho * scale))) + (incrementadorx * scale), (j * ((altoPieza * scale) - (nMediaNilAlto * scale))) + (incrementadory * scale), ((anchoPieza * scale) - (nMediaNilAncho * scale)), ((altoPieza * scale) - (nMediaNilAlto * scale)));
+      incrementadory += (_medianilAlto / 2) * 1;
+      doc.setLineWidth(0.2);
+      doc.rect((i * ((_anchoPieza * scale) - (nMediaNilAncho * scale))) + (incrementadorx * scale), (j * ((_altoPieza * scale) - (nMediaNilAlto * scale))) + (incrementadory * scale), ((_anchoPieza * scale) - (nMediaNilAncho * scale)), ((_altoPieza * scale) - (nMediaNilAlto * scale)));
     }
   }
-  const pdf = doc.output();
-  doc.save("varela.pdf");
-  return pdf;
+  const nombreArchivo = `files/${uid(16)}.pdf`;
+  doc.save(nombreArchivo);
+  return nombreArchivo;
+
 }
 
-const ImpresionFlexible = () => {
-  const enAncho = false;
-  const alto = 110;
-  const ancho = 152;
+const ImpresionFlexible = ({ enAncho, ancho, alto }, { medianilAncho, medianilAlto, anchoPieza, altoPieza }) => {
+  console.log({ anchoPieza, altoPieza })
+  const _enAncho = enAncho;//false;
+  const _alto = 80;
+  const _ancho = ancho;
 
-  const medianilAncho = 1;
-  const medianilAlto = 1;
+  const _medianilAncho = medianilAncho;
+  const _medianilAlto = medianilAlto;
 
-  let anchoPieza = 10;
-  let altoPieza = 10;
+  let _anchoPieza = anchoPieza;
+  let _altoPieza = altoPieza;
 
-  const nMediaNilAncho = medianilAncho / 2;
-  const nMediaNilAlto = medianilAlto / 2;
+  const nMediaNilAncho = _medianilAncho / 2;
+  const nMediaNilAlto = _medianilAlto / 2;
 
 
-  if (!enAncho) {
-    const t = anchoPieza;
-    anchoPieza = altoPieza;
-    altoPieza = t;
+  if (!_enAncho) {
+    const t = _anchoPieza;
+    _anchoPieza = _altoPieza;
+    _altoPieza = t;
   }
   let orientation = "p";
-  if (alto < ancho) {
+  if (_alto < _ancho) {
     orientation = "l";
   }
 
-  const doc = new jsPDF(orientation, 'cm', [alto, ancho]);
-  const itemsAncho = Math.trunc(ancho / anchoPieza);
+  const doc = new jsPDF(orientation, 'cm', [_alto, _ancho]);
+  const itemsAncho = Math.trunc(_ancho / _anchoPieza);
 
 
   doc.setFont('times', 'bold');
-  doc.setFontSize(75);
-  doc.text(ancho / 2, 5, `${ancho} cm  - (${itemsAncho} columnas) `, { align: "center" });
+  doc.setFontSize(100);
+  doc.text(ancho / 2, 5, `${ancho} cm  - (${itemsAncho} columnas) `, { align: "center" });  
   doc.text(5, alto / 2, `Material Flexible o Fotografico `, { angle: 90, align: "left" });
   doc.setDrawColor(98, 5, 100,);
   let incrementadorx = 0;
   for (let i = 0; i <= (itemsAncho - 1); i++) {
     incrementadorx += nMediaNilAncho;
-    doc.rect((i * (anchoPieza - nMediaNilAncho)) + (incrementadorx), (alto - (altoPieza + 3)), (anchoPieza - nMediaNilAncho), (altoPieza - nMediaNilAlto));
+    doc.rect((i * (_anchoPieza - nMediaNilAncho)) + (incrementadorx), (_alto - (_altoPieza + 3)), (_anchoPieza - nMediaNilAncho), (_altoPieza - nMediaNilAlto));
   }
-  const pdf = doc.output();
-  doc.save("imagen.pdf");
-  return pdf;
-  //res.contentType("application/pdf");
-  //res.end(pdf)
+  const nombreArchivo = `files/${uid(16)}.pdf`;
+  doc.save(nombreArchivo);
+  return nombreArchivo;
+
 }
 
 
-async function base64_encode(file) {
 
-  const fs = require('fs').promises;
-  const contents = await fs.readFile(file, { encoding: 'base64' });
-  return contents;
+function formatearInformacion(material) {
+  const item = {
+    anchoPieza: material.ANCHO,
+    altoPieza: material.ALTO,
+    medianilAncho: material.MEDIANIL_ANCHO,
+    medianilAlto: material.MEDIANIL_ALTO
+  }
+  let ListaMateriales = [];
+  if (material.ID_MATERIAL != 0) {
+    ListaMateriales.push({
+      esRigido: material.tipoMaterial == "R",
+      descripcion: material.descripcionMaterial,
+      enAncho: material.ORIENTA == "A lo ancho",
+      ancho: material.MATANCHO,
+      alto: material.MATALTO
+    })
+  }
+  if (material.ID_MATERIAL2 != 0) {
+    ListaMateriales.push({
+      esRigido: material.tipoMaterial2 == "R",
+      descripcion: material.descripcionMaterial2,
+      enAncho: material.ORIENTA2 == "A lo ancho",
+      ancho: material.MATANCHO2,
+      alto: material.MATALTO2,
+
+    })
+  }
+  if (material.ID_MATERIAL3 != 0) {
+    ListaMateriales.push({
+      esRigido: material.tipoMaterial3 == "R",
+      descripcion: material.descripcionMaterial3,
+      enAncho: material.ORIENTA3 == "A lo ancho",
+      ancho: material.MATANCHO3,
+      alto: material.MATALTO3,
+    })
+  }
+  if (material.ID_MATERIAL4 != 0) {
+    ListaMateriales.push({
+      esRigido: material.tipoMaterial4 == "R",
+      descripcion: material.descripcionMaterial4,
+      enAncho: material.ORIENTA4 == "A lo ancho",
+      ancho: material.MATANCHO4,
+      alto: material.MATALTO4,
+
+    })
+  }
+  return { item, ListaMateriales };
+}
+
+
+function removerArchivos() {
+  const fs = require('fs');
+  const path = require('path');
+  const directory = 'files';
+
+  fs.readdir(directory, (err, files) => {
+    if (err) throw err;
+    for (const file of files) {
+      fs.unlink(path.join(directory, file), err => {
+        if (err) throw err;
+      });
+    }
+  });
+
 }
 
 
 const Imprimir = async (req, res = response) => {
+  const MaterialDAO = require("../DAO/Material");
+  const materialDAO = new MaterialDAO();
+  const [material] = await materialDAO.porID("49226");
+  if (material == undefined) {
+    return res.json({
+      "mensaje": "No se encontro informacion de este folio"
+    })
+  }
+  const { item, ListaMateriales } = formatearInformacion(material);
   //const respuestaBuffer =await httpGet("http://192.168.2.222/litoapps/cotizador/dompdf/cotizacion.php?foliob=49226")
-  const rigido =ImpresionRigido();
-  ImpresionFlexible()
-  //const buffer=Buffer.from(rigido,'binary');
-
-
+  //Primero se verifica si es rigido o flexible para llamar a los diferentesModulos
   //const merged = await merge([respuestaBuffer,buffer]);
-  res.contentType("application/pdf");
-
-
   const PDFImage = require("pdf-image").PDFImage;
-
-  //rigido.save("varela.pdf")
-  let pdfImage = new PDFImage("varela.pdf");
-  const image = await pdfImage.convertPage(0);
-
-  let pdfImage1 = new PDFImage("imagen.pdf");
-  const image1 = await pdfImage1.convertPage(0);
-
   const PDFGenerator = require('pdfkit')
-
   let doc = new PDFGenerator();
-  doc.text("Primer material");
-  doc.image(image, { fit: [100, 100] })
-  doc.text("Segundo material");
-  doc.image(image1, { fit: [100, 100] })
+  console.log(ListaMateriales);
+  for (const _m of ListaMateriales) {
+    let nombreArchivo = "";
+    if (_m.esRigido) {
+      nombreArchivo = ImpresionRigido(_m, item);
+    } else {
+      nombreArchivo = ImpresionFlexible(_m, item);
+    }    
+    let pdfImage = new PDFImage(nombreArchivo);
+    const archivoImagen = await pdfImage.convertPage(0);    
+    doc.text(`Material ${_m.descripcion}`);
+    doc.image(archivoImagen, { fit: [250, 250] })
+  }
 
-
-
-
-
-
+  res.contentType("application/pdf");
   doc.pipe(res);
   doc.end()
-  
-
-
+  removerArchivos();
 }
 
 
