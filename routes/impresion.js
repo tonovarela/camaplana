412457963f20a6711b6066a700d063fc8b5cbebe
tonovarela <tonovarela@live.com>
2022-07-05 09:17:06 -0500

    fixes menores"

--- routes/impresion.js
+++ routes/impresion.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 const  { Imprimir} = require("../controllers/impresion");
 
 const router = Router();
-
-router.get("/imprimir/:folio",Imprimir);
+//50253
+router.get("/:folio",Imprimir);
 
 module.exports= router;
